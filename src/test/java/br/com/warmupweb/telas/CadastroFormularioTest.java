3802078d82f401c1b3bac6c8e4044a16e2d8756e
Ricardo Varela <ricardomvvarela@gmail.com>
2023-03-28 21:17:23 -0300

    Versão 1.0.0

---
+++ src/test/java/br/com/warmupweb/telas/CadastroFormularioTest.java
@@ -0,0 +1,64 @@
+package br.com.warmupweb.telas;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+public class CadastroFormularioTest {
+
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("remote-allow-origins=*");
+        System.setProperty("webdriver.chrome.driver", "C:/Driver/chromedriver.exe");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://warmupweb.com.br");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+
+    }
+
+    @Test
+    public void CadastroValidoFormulario() throws InterruptedException {
+        //Preenchimento de formulário de cadastro
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,2700)", "");
+        Thread.sleep(2000);
+        driver.findElement(By.id("wpforms-25625-field_5")).sendKeys("Teste");
+        driver.findElement(By.id("wpforms-25625-field_2")).sendKeys("testeautomatizado@yopmail.com");
+        driver.findElement(By.id("wpforms-25625-field_9")).sendKeys("Teste Automatizado WarmUP");
+        driver.findElement(By.id("wpforms-25625-field_3")).sendKeys("84990909090");
+        Thread.sleep(3000);
+
+        //Dropdown - Dinâmico
+        driver.findElement(By.xpath("//div[@class='choices__item choices__placeholder choices__item--selectable']")).click();
+        driver.findElement(By.xpath("//input[@aria-label='Célula de Testes']")).sendKeys("Automação de Testes");
+        driver.findElement(By.id("choices--wpforms-25625-field_4-item-choice-3")).click();
+
+        driver.findElement(By.id("wpforms-25625-field_6_1")).click();
+        driver.findElement(By.id("wpforms-25625-field_7")).sendKeys("Estão sendo realizados testes automatizados no formulário, obrigado.");
+        js.executeScript("window.scrollBy(0,300)", "");
+        Thread.sleep(2000);
+        driver.findElement(By.id("wpforms-submit-25625")).click();
+        Thread.sleep(5000);
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"wpforms-confirmation-25625\"]/p")).getText(), "Teste obrigado por entrar em contato conosco! Entraremos em contato contigo em breve.");
+    }
+
+}
